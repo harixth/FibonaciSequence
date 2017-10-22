245c03c1efe71eeaf69bd79b2853831ba25caebb
harixth <harixth@yahoo.com>
2017-10-22 21:47:45 +0800

    Add project files.

---
+++ FibonaciSequence/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonaciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 0, x = 1, y = 2, sum=2;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            while (num < 4000000)
+            {
+                num = x + y;
+                if (num%2==0)
+                {
+                    sum = sum + num;
+                }
+                x = y;
+                y = num;
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine($"Sum of the even number is {sum}");
+        }
+    }
+}
